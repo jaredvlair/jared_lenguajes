6ef0ab0d766950fc68fb9ed890362312cc2cf01d
jared <jared@DESKTOP-865SHKP>
2017-09-18 19:39:45 -0500

    pues ya cabe , debo repasar

--- lenguajes/src/abstraccion/Imc.java
+++ lenguajes/src/abstraccion/Imc.java
@@ -12,5 +12,11 @@ package abstraccion;
 public class Imc {
     usuario u;
     String resultado;
+    public String  calcular (){
+        float mivalor =u.peso/(u.altura*u.altura);
+        resultado="Tu imc es "+mivalor;
+        return resultado;
+                
+    }
     
 }
